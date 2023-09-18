1cde38cb58aceb69f22fb5dea31c88b980f7214d
innaaa1 <innaa.boikoo@gmail.com>
2023-09-18 16:42:06 -0400

    add constructor

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,16 @@
+import java.util.List;
+
 public class Tree {
+
+    private Node root;
+    private Tree subtree;
+
+    public Tree(Node root, Tree subtree, List<Tree> list){
+        this.root = root;
+        if (subtree == null){
+            this.subtree = subtree;
+        }
+
+    }
+
 }
